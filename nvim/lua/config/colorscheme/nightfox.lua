23694d0433fb8c4a30db96e7b458c0ea2f1088b8
haidangnng <haidangnng@gmail.com>
2026-09-21 03:00:29 +0200

    update: from scratch

--- nvim/lua/config/colorscheme/nightfox.lua
+++
@@ -1,9 +0,0 @@
-require("nightfox").setup({
-	options = {
-		-- Compiled file's destination location
-		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-		transparent = true,
-	},
-})
-
-vim.cmd("colorscheme carbonfox")
