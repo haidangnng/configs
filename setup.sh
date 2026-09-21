7016c55f623f70a579756501d74fc9cdd9cc506b
haidangnng <haidangnng@gmail.com>
2026-09-21 14:45:01 +0200

    update: workspace

--- setup.sh
+++ setup.sh
@@ -54,26 +54,20 @@ read -r -d '' PACKAGE_LIST <<'EOF'
 brew install \
   yazi \
   nvm \
-  pnpm \
   neovim \
   ripgrep \
   eza \
   atuin \
   zoxide \
-  jandedobbeleer/oh-my-posh/oh-my-posh \
-  pyenv
+  jandedobbeleer/oh-my-posh/oh-my-posh
 
 brew install --cask \
   raycast \
-  bitwarden \
   google-chrome  \
   firefox \
   wezterm \
   visual-studio-code \
   docker \
-  discord \
-  keycastr \
-  notunes \
   mos \
   spotify \
   nikitabobko/tab/aerospace
